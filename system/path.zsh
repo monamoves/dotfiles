de952361a4d131a2f2c4c8505e6bd67bd5f96970
Peter Major <pmajor@sankynet.com>
2013-05-30 17:37:15 -0400

    Update config files

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,4 @@
-export PATH="$DOTS/bin:$HOME/.rvm/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/usr/X11/bin"
+export PATH=$DOTS/bin:/usr/local/bin:/usr/local/sbin:$PATH
+#export PATH="$DOTS/bin:$HOME/.rvm/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/usr/X11/bin"
 
-source $(rvm 1.9.3 do rvm env --path)
+#source $(rvm 1.9.3 do rvm env --path)
